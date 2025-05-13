f17f90c6cf2f8df3699da84e49164d0211ca1b44
Nathaniel Kiwa <nhlanhlakiwa@gmail.com>
2025-05-13 14:39:36 +0200

    Add files via upload

---
+++ Remove-anchor-from-URL.js
@@ -0,0 +1,13 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+// my solution
+
+function removeUrlAnchor(url){
+  // TODO: complete
+
+  url.split('#')[0]
+}
