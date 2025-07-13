fcfdeb44f6be0a2e246f762b22486976ab742352
Nathaniel Kiwa <nhlanhlakiwa@gmail.com>
2025-07-13 18:26:30 +0200

    Add files via upload

---
+++ Function-squaring-an-argument2.js
@@ -0,0 +1,14 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+//my solution 
+
+function square (n){
+  return n**2
+}
+
+// 2nd solution
+
+function square(num){
+  let num = Math.pow(num, 2); //Math.pow(x,y) = x^y
+  return num;
+};
