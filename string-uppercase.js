6571483037dc0e5a56bf6843ed23e960a9a98ecc
Nathaniel Kiwa <nhlanhlakiwa@gmail.com>
2025-07-31 19:07:43 +0200

    Add files via upload

---
+++ string-uppercase.js
@@ -0,0 +1,18 @@
+// Task
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples (input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+// solution 
+
+String.prototype.isUpperCase = function() {
+  // your code here
+
+  return this.toUpperCase() === this.toString()
+}
