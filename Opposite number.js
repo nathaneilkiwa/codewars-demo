71900c7de2c80bb5b2f5d9f07d73de0aca8465f3
Nathaniel Kiwa <nhlanhlakiwa@gmail.com>
2026-01-13 17:08:20 +0200

    Add files via upload

---
+++ Opposite number.js
@@ -0,0 +1,17 @@
+/*Very simple, given a number, find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34 */
+function opposite(number) {
+    return -number;
+}
+
+// Alternative solution using multiplication
+/*
+function opposite(number) {
+    return number * -1;
+}
+*/
